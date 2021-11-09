81860c3330bce1db848d213dd7b3b7a1f276f542
ctownsdin <townsdin@gmail.com>
2021-11-09 12:43:46 -0800

    .

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("simple-express root");
+});
+
+app.get("/health", (req, res, next) => {
+  res.status(200).send("Hello Health!");
+});
